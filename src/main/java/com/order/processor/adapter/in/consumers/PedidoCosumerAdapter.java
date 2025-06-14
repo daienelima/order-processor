fc525654eb7d44015af53c2b614a2fd2e9be79c2
daienelima <daiene.m17@gmail.com>
2025-06-14 11:11:34 -0300

    Add teste de integtacao kafka

--- src/main/java/com/order/processor/adapter/in/consumers/PedidoCosumerAdapter.java
+++ src/main/java/com/order/processor/adapter/in/consumers/PedidoCosumerAdapter.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PedidoCosumerAdapter {
 
-    private final ProcessarPedidoInputPort processarPedTidoInputPort;
+    private final ProcessarPedidoInputPort processarPedidoInputPort;
 
     public PedidoCosumerAdapter(ProcessarPedidoInputPort processarPedidoInputPort) {
         this.processarPedidoInputPort = processarPedidoInputPort;
