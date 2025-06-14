33026d61f6eacf08ba902da8c19f5a3d7d258138
daienelima <daiene.m17@gmail.com>
2025-06-14 13:27:22 -0300

    testes unitarios

--- src/main/java/com/order/processor/config/KafkaConsumerConfig.java
+++ src/main/java/com/order/processor/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.order.processor.config;
 
 import com.order.processor.adapter.in.consumers.message.PedidoMensagem;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,10 +20,13 @@ import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, PedidoMensagem> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
